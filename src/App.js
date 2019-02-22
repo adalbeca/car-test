97ce59f940dde567f8954f545092d952611d2948
Adelys <adalbeca@gmail.com>
2019-02-22 17:01:30 -0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-
+import Header from './components/Header';
+import Body from './components/Body';
+import { Container, Row, Col } from 'reactstrap';
 class App extends Component {
   render() {
     return (
-     <div>Test</div>
+      <Container fluid>
+        <Row>
+          <Col>
+                <Header/>
+                <Body/>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
